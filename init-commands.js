0f72fad307bd5a0a05c12f489bd58b07e8f0972a
ZS01 <ZShadow01>
2022-09-18 22:51:07 +0200

    Removed guild id

--- init-commands.js
+++ init-commands.js
@@ -3,14 +3,14 @@ const { REST, Routes } = require('discord.js');
 
 require('dotenv').config();
 const TOKEN = process.env.SURTR;
+const TEST_GUILD_ID = process.env.TEST_GUILD_ID;
 
 const CLIENT_ID = '842830194346950696';
-const GUILD_ID = '842833941433286707';
 
 const rest = new REST({version: '10'}).setToken(TOKEN);
 
 
-// FETCH COMMANDS /////////////////////////////////////////////////////////////////////////////////
+// Fetch commands
 const commands = [];
 const commandsPath = __dirname + '/commands';
 const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
@@ -21,13 +21,13 @@ for (const cmdFile of commandFiles) {
 }
 
 
-// DEPLOY COMMANDS ////////////////////////////////////////////////////////////////////////////////
+// Deploy commands
 (async () => {
     try {
         console.log("Started refreshing application (/) commands.");
 
         // await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });  // Applies commands to all guilds
-        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });  // Only applices commands to GUILD_ID
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), { body: commands });  // Only applies commands to GUILD_ID
         
         console.log("Successfully reloaded application (/) commands.");
     } catch (error) {
